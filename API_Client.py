71b9ca96cb26620e11e177d60a27d90837388298
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2020-04-30 16:54:12 +0800

    Update API_Client.py

--- API_Client.py
+++ API_Client.py
@@ -2,13 +2,14 @@ import requests
 import base64
 import json
  
-url = input()
+url = input('请输入API的URL:')
 #url = 'http://192.18.31.31:5000/'
+
 try:
     files = {"pic":open("2.jpg","rb")}
     response = requests.post(url,files = files)
     print('上传成功')
-    print(response.text)
+    print(json.loads(response.text))
 except:
-    print('上传失败，请重新检查')
+    print('上传失败，请重新检查重试操作')
 
